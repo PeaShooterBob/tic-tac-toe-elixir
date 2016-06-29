4e7cf1aadfef6cb3780e4b3ee76ae3d5c082e059
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-06-29 15:41:55 -0700

    Game mode checks for valid game mode

--- lib/tic_tac_toe_elixir/validators/tic_tac_toe.ex
+++ lib/tic_tac_toe_elixir/validators/tic_tac_toe.ex
@@ -1,12 +1,16 @@
 defmodule TicTacToeElixir.Validators.TicTacToe do
   alias TicTacToeElixir.Games.Marker, as: Marker
 
-  def valid_move?(move, board) do
+  def move?(move, board) do
     !marker?(move) && move_on_board?(move, board)
   end
 
-  defp marker?(move) do
+  def marker?(move) do
     move == Marker.x_marker || move == Marker.o_marker
+  end
+
+  def game_mode?(game_mode) do
+    game_mode == "HH" || game_mode == "HC"
   end
 
   defp move_on_board?(move, board) do
--- test/validator_test.exs
+++ test/validator_test.exs
@@ -3,25 +3,53 @@ defmodule ValidatorTest do
   alias TicTacToeElixir.Validators.TicTacToe, as: Validator
   alias TicTacToeElixir.Games.Marker, as: Marker
 
-  describe "Validator.valid_move?/2" do
+  describe "Validator.move?/2" do
     test "returns true if move is valid" do
       board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-      assert Validator.valid_move?(8, board) == true
+      assert Validator.move?(8, board) == true
     end
 
     test "returns false if move is not valid" do
       board = [Marker.x_marker, 1, 2, 3, 4, 5, 6, 7,8]
-      assert Validator.valid_move?(0, board) == false
+      assert Validator.move?(0, board) == false
     end
 
     test "returns false if move is not part of board" do
       board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-      assert Validator.valid_move?(200, board) == false
+      assert Validator.move?(200, board) == false
     end
 
     test "returns false if move is a marker" do
       board = [Marker.x_marker, 1, 2, 3, 4, 5, 6, 7, 8]
-      assert Validator.valid_move?(Marker.x_marker, board) == false
+      assert Validator.move?(Marker.x_marker, board) == false
+    end
+  end
+
+  describe "Validator.marker?/1" do
+    test "returns true for x marker" do
+      assert Validator.marker?(Marker.x_marker) == true
+    end
+
+    test "returns true for o marker" do
+      assert Validator.marker?(Marker.o_marker) == true
+    end
+
+    test "returns false for invalid marker" do
+      assert Validator.marker?("Rook") == false
+    end
+  end
+
+  describe "Valdiator.game_mode?/1" do
+    test "returns true for human vs human game mode" do
+      assert Validator.game_mode?("HH") == true
+    end
+
+    test "returns true for human vs computer game mode" do
+      assert Validator.game_mode?("HC") == true
+    end
+
+    test "returns false for invalid game mode" do
+      assert Validator.game_mode?("PP") == false
     end
   end
 end
