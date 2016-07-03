e0136ab94e91b4fab0d8ad0b54123c1351e69380
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-07-03 10:47:56 -0700

    Minimax searches to a certain depth

--- lib/tic_tac_toe_elixir/strategies/minimax.ex
+++ lib/tic_tac_toe_elixir/strategies/minimax.ex
@@ -1,6 +1,5 @@
 defmodule TicTacToeElixir.Strategies.Minimax do
   alias TicTacToeElixir.Games.Marker, as: Marker
-  require IEx
 
   @behaviour TicTacToeElixir.Strategies.Strategy
 
@@ -11,18 +10,19 @@ defmodule TicTacToeElixir.Strategies.Minimax do
     |> elem(0)
   end
 
-  defp minimax(marker, board, maximizing_player, rules, board_evaluator) do
-    case rules.over?(board, board_evaluator) do
+  defp minimax(marker, board, maximizing_player, original_depth, depth, rules, board_evaluator) do
+    case rules.over?(board, board_evaluator) ||
+    serach_depth_reached?(original_depth, depth) do
       true -> score(board, maximizing_player, rules, board_evaluator)
       false ->
         marker = opposite_marker(marker)
         maximizing_player = !maximizing_player
         case maximizing_player do
           true ->
-            children_board_scores(marker, board, maximizing_player, rules, board_evaluator)
+            children_board_scores(marker, board, maximizing_player, original_depth, rules, board_evaluator)
             |> Enum.max
           false ->
-            children_board_scores(marker, board, maximizing_player, rules, board_evaluator)
+            children_board_scores(marker, board, maximizing_player, original_depth, rules, board_evaluator)
             |> Enum.min
         end
     end
@@ -30,7 +30,8 @@ defmodule TicTacToeElixir.Strategies.Minimax do
 
   defp move_scores(marker, board, rules, board_evaluator) do
     empty_spaces = empty_spaces(board, board_evaluator)
-    scores = children_board_scores(marker, board, true, rules, board_evaluator)
+    original_depth = depth(board, board_evaluator)
+    scores = children_board_scores(marker, board, true, original_depth, rules, board_evaluator)
     Enum.zip(empty_spaces, scores)
   end
 
@@ -40,23 +41,33 @@ defmodule TicTacToeElixir.Strategies.Minimax do
     end)
   end
 
+  defp children_board_scores(marker, board, maximizing_player, original_depth, rules, board_evaluator) do
+    empty_spaces = empty_spaces(board, board_evaluator)
+    Enum.map(empty_spaces, fn (space) ->
+      board = List.replace_at(board, space, marker)
+      depth = depth(board, board_evaluator)
+      minimax(marker, board, maximizing_player, original_depth, depth, rules, board_evaluator)
+    end)
+  end
+
   defp score(board, maximizing_player, rules, board_evaluator) do
-    case rules.tied?(board, board_evaluator) do
-      true -> 0
-      false ->
+    case rules.won?(board, board_evaluator) do
+      true ->
         case maximizing_player do
           true -> 10
           false -> -10
         end
+      false -> 0
     end
   end
 
-  defp children_board_scores(marker, board, maximizing_player, rules, board_evaluator) do
+  defp serach_depth_reached?(original_depth, depth) do
+    original_depth - depth > 4
+  end
+
+  defp depth(board, board_evaluator) do
     empty_spaces = empty_spaces(board, board_evaluator)
-    Enum.map(empty_spaces, fn (space) ->
-      board = List.replace_at(board, space, marker)
-      minimax(marker, board, maximizing_player, rules, board_evaluator)
-    end)
+    length(empty_spaces)
   end
 
   defp opposite_marker(marker) do
--- test/minimax_test.exs
+++ test/minimax_test.exs
@@ -7,27 +7,42 @@ defmodule MinimaxTest do
 
   describe "Minimax.best_move/3" do
     test "blocks winning move" do
-      board = ["X", "X", 2, 3, 4, 5, 6, 7, 8]
-      best_move = Minimax.best_move("O", board, Rules, Evaluator)
+      board = [Marker.x_marker, Marker.x_marker, 2, 3, 4, 5, 6, 7, 8]
+      best_move = Minimax.best_move(Marker.o_marker, board, Rules, Evaluator)
       assert best_move == 2
     end
 
     test "blocks a two way fork for opponent" do
-      board = [0, "X", 2,
-              "X", "O", 5,
+      board = [0, Marker.x_marker, 2,
+              Marker.x_marker, Marker.o_marker, 5,
                6, 7, 8]
-     best_move = Minimax.best_move("O", board, Rules, Evaluator)
+     best_move = Minimax.best_move(Marker.o_marker, board, Rules, Evaluator)
      assert best_move == 0
     end
 
     test "does not create a two way unblocked fork for opponent" do
-      board = [ 0, 1, "X",
-                3, "O", 5,
-               "X", 7, 8]
-      best_move = Minimax.best_move("O", board, Rules, Evaluator)
+      board = [ 0, 1, Marker.x_marker,
+                3, Marker.o_marker, 5,
+               Marker.x_marker, 7, 8]
+      best_move = Minimax.best_move(Marker.o_marker, board, Rules, Evaluator)
       assert Enum.member?([1, 3, 5, 7], best_move)
+    end
+
+    test "picks best move quickly" do
+      board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      time_to_run = Benchmark.measure(fn -> Minimax.best_move(Marker.x_marker, board, Rules, Evaluator) end)
+      assert time_to_run < 4
     end
   end
 
+end
+
+defmodule Benchmark do
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
 end
 
